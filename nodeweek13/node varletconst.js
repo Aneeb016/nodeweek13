b3b3f627f74d1c2cf0dfd0071d781db34f97ef39
Aneeb016 <163635531+Aneeb016@users.noreply.github.com>
2024-05-15 19:01:42 +0500

    Add files via upload

---
+++ nodeweek13/node varletconst.js
@@ -0,0 +1,11 @@
+var name = "Aneeb";
+let age = 10;
+const salary =  100;
+//let age = 10; we cant use it again
+ //salary = 20;
+ console.log(salary);
+ //var name = "aneeb";
+
+ var num1 = 100;
+ var num2 = 2.5;
+ console.log(name);
